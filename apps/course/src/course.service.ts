16b97b565844b5e90faaa87ed38205c75bf48b90
abenezer <105964367+abenezer444@users.noreply.github.com>
2024-04-05 13:51:44 +0300

    Update course.service.ts

--- apps/course/src/course.service.ts
+++ apps/course/src/course.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Course } from './course.model';
+import { Teacher } from './teacher.model';
+import { Student } from './student.model';
 
 @Injectable()
 export class CourseService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectModel('Course') private readonly courseModel: Model<Course>) {}
+
+  async getAllCourses(): Promise<Course[]> {
+    return this.courseModel.find().exec();
+  }
+
+  async getCourseTeacher(courseId: string): Promise<Teacher> {
+    const course = await this.courseModel.findById(courseId).exec();
+    return course.teacher;
+  }
+
+  async getCourseStudents(courseId: string): Promise<Student[]> {
+    const course = await this.courseModel.findById(courseId).exec();
+    return course.students;
+  }
+
+  async createCourse(courseData: Course): Promise<Course> {
+    const createdCourse = new this.courseModel(courseData);
+    return createdCourse.save();
   }
 }
